85ed5a49541df8ba754d9a7dbbe11f29e0c732e3
Linda Loftsgarden <linda-fintlabs@users.noreply.github.com>
2022-12-13 19:52:56 +0100

    Added dropdown for filter on unit

---
+++ src/features/users/FilterUnitUser.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+
+export default function FilterUnitUser() {
+
+    function handleChange() {
+    }
+
+    let unit;
+    return (
+        <FormControl style={{minWidth: 220}} sx={{mx: '2rem'}}>
+            <InputLabel id="demo-simple-select-label">Enhet</InputLabel>
+            <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select-autowidth"
+                value={unit}
+                label="Enhet"
+                onChange={handleChange}
+            >
+                <MenuItem value={1}>Viken videregående skole</MenuItem>
+                <MenuItem value={2}>Glemmen videregående skole</MenuItem>
+                <MenuItem value={3}>Drammen videregående skole</MenuItem>
+            </Select>
+        </FormControl>
+    );
+}
