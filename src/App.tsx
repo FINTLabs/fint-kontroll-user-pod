8dd1b4e90d5862899f6a7a3cabf0e56dfda7c875
jennifer-fintlabs <jennifer@fintlabs.no>
2023-11-01 09:44:12 +0100

    fix basepath

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import theme from './template/theme';
 import {ThemeProvider} from '@mui/material/styles';
 import UsersProvider from "./context/userContext/UsersContext";
 import RouteList from "./features/routes/RouteList";
+import {BasePathProvider} from "./context/BasePathContext";
 
 function App() {
 
     return (
         <ThemeProvider theme={theme}>
+            <BasePathProvider>
             <UsersProvider>
                 <RouteList/>
             </UsersProvider>
+            </BasePathProvider>
         </ThemeProvider>
     );
 
---
+++ src/context/BasePathContext.tsx
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import axios from 'axios';
+
+type BasePathContextType = string | undefined; // Define the type of the context value
+
+const BasePathContext = createContext<BasePathContextType | undefined>(undefined);
+
+export const useBasePath = (): BasePathContextType => {
+    return useContext(BasePathContext);
+};
+
+interface BasePathProviderProps {
+    children: ReactNode;
+}
+
+export const BasePathProvider: React.FC<BasePathProviderProps> = ({ children }) => {
+    const [basePath, setBasePath] = useState<string>('');
+
+    useEffect(() => {
+        const getBasePath = () => {
+            axios.get('api/layout/configuration')
+                .then(response => {
+                    setBasePath(response.data.basePath);
+                    console.log("basePath in context", response.data.basePath);
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        };
+
+        if (process.env.NODE_ENV === 'production') {
+            getBasePath();
+        }
+    }, []);
+
+    return (
+        <BasePathContext.Provider value={basePath}>
+            {children}
+        </BasePathContext.Provider>
+    );
+};
