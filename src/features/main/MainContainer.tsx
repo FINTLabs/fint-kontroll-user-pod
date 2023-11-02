c439f92c4e46764b4722d6098371344420336c10
jennifer-fintlabs <jennifer@fintlabs.no>
2023-11-02 15:32:21 +0100

    added basepath to context

--- src/features/main/MainContainer.tsx
+++ src/features/main/MainContainer.tsx
@@ -3,13 +3,15 @@ import {UserTable} from "./UserTable";
 import * as React from "react";
 import style from "../../template/style";
 import UsersProvider from "../../context/userContext";
+import { useBasePath } from '../../context/BasePathContext';
 
 function MainContainer() {
+    const basePath = useBasePath() || '';
 
     return (
         <Box sx={style.content}>
             <Box sx={style.table}>
-                <UsersProvider>
+                <UsersProvider basePath={basePath}>
                 <UserTable/>
                 </UsersProvider>
             </Box>
